707f402e3839a3eeb5c1128c5e6ce58dd1e8bd5c
Albert <albert.barsamov@gmail.com>
2022-02-24 16:17:17 +0100

    Navbar mvp

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -11,14 +11,14 @@ export default function Navbar() {
 
     return (
         <nav className="navbar ">
-            <div className='navbar_logo'>
-                <img src={logo} className='navbar_logo' alt="" />
+            <div className='navbar_brand'>
+                <img src={logo} className='navbar_logo' alt="brand logo" />
                 <h1><span>Cloud</span><span>Disk</span></h1>
             </div>
             <div className='navbar_btns'>
-                <button className="btn ">Sign Up</button>
-                <button className="btn ">Sign In</button>
-                <button className="btn ">Log out</button>
+                <button className="navbar_btn">Sign Up</button>
+                <button className="navbar_btn">Sign In</button>
+                <button className="navbar_bnt">Log out</button>
             </div>
         </nav>
     )
