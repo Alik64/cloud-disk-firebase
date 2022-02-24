dfcf5234b85ebf8703e008fa566160c9b7c93ed9
Albert <albert.barsamov@gmail.com>
2022-02-24 22:51:32 +0100

    create navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,13 +1,31 @@
+
+import { signInToggle, signUpToggle, modalClose } from '../../redux/modalReducer'
+import { useDispatch, useSelector } from 'react-redux'
+import { useState } from 'react'
+
 import './navbar.css'
 import logo from '../../assets/images/cloud-cd-logo.png'
+import menu from '../../assets/images/mobil-menu.svg'
+import { useWindowSize } from '../../utils/custom-hooks';
+
+
 
 
 
 
 
 export default function Navbar() {
+    const modalIn = useSelector(state => state.modal.signInModal)
+    const modalUp = useSelector(state => state.modal.signUpModal)
+    const [menuState, setMenuState] = useState(false)
+
+    const toggleMenu = () => {
+        setMenuState(!menuState)
+    }
 
 
+    const size = useWindowSize();
+    const dispatch = useDispatch()
 
     return (
         <nav className="navbar ">
@@ -15,11 +33,26 @@ export default function Navbar() {
                 <img src={logo} className='navbar_logo' alt="brand logo" />
                 <h1><span>Cloud</span><span>Disk</span></h1>
             </div>
-            <div className='navbar_btns'>
-                <button className="navbar_btn">Sign Up</button>
-                <button className="navbar_btn">Sign In</button>
-                <button className="navbar_bnt">Log out</button>
+            <div className={size.width < 480 ? "navbar_mobil visible" : "navbar_mobil hide"}>
+                <img onClick={toggleMenu}
+                    src={menu} alt="" />
             </div>
-        </nav>
+            <div className={size.width < 480 ? "navbar_menu mobile" : "navbar_menu"}>
+
+
+                <div>
+                    {modalIn && <h2>modalIn</h2>}
+                    {modalUp && <h2>modalUp</h2>}
+
+                </div>
+                <div className={size.width < 480 ? "navbar_btns mobile" : "navbar_btns"}
+                    style={{ display: menuState || size.width > 480 ? "flex" : "none" }}>
+                    <button onClick={() => dispatch(signUpToggle(true))} className="navbar_btn">Sign Up</button>
+                    <button onClick={() => dispatch(signInToggle(true))} className="navbar_btn">Sign In</button>
+                    <button onClick={() => dispatch(modalClose(false))} className="navbar_btn">Log out</button>
+                </div>
+            </div>
+        </nav >
     )
 }
+
