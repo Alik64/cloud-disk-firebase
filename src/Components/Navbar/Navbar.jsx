a54b314d9cbe3cd9a094d1f789c4117f9d5e2451
Albert <albert.barsamov@gmail.com>
2022-03-03 16:41:17 +0100

    set Navbar css width

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,22 +1,22 @@
-
+import { useState } from 'react'
+import './navbar.css'
 
 import { useDispatch, useSelector } from 'react-redux'
-import { useState } from 'react'
+
 import { Link } from 'react-router-dom'
-import './navbar.css'
+
 import logo from '../../assets/images/cloud-cd-logo.png'
 import menu from '../../assets/images/mobil-menu.svg'
+
 import { useWindowSize } from '../../utils/custom-hooks';
-import { signInToggle, signUpToggle, modalClose } from '../../redux/modalReducer'
-
-
-
-
+import { signInToggle, signUpToggle } from '../../redux/modalReducer'
+import { logOut } from '../../Firebase/firebase-config'
 
 
 
 export default function Navbar() {
-
+    const currentUser = useSelector(state => state.user.currentUser)
+    const loadingData = useSelector(state => state.user.loadingData)
 
     const [menuState, setMenuState] = useState(false)
 
@@ -29,8 +29,8 @@ export default function Navbar() {
     const dispatch = useDispatch()
 
     return (
-        <nav className="navbar app_wrapper">
-            <Link to="/">
+        <nav className="navbar ">
+            <Link to="/private/private-home">
                 <div className='navbar_brand'>
                     <img src={logo} className='navbar_logo' alt="brand logo" />
                     <h1><span style={{ color: "var(--blue)" }} >Cloud</span><span style={{ color: "var(--green)" }} >Disk</span></h1>
@@ -42,17 +42,26 @@ export default function Navbar() {
             </div>
             <div className={size.width < 480 ? "navbar_menu mobile" : "navbar_menu"}>
 
-
-                <div>
-
-
-
-                </div>
                 <div className={size.width < 480 ? "navbar_btns mobile" : "navbar_btns"}
                     style={{ display: menuState || size.width > 480 ? "flex" : "none" }}>
-                    <button onClick={() => dispatch(signUpToggle(true))} className="navbar_btn">Sign Up</button>
-                    <button onClick={() => dispatch(signInToggle(true))} className="navbar_btn">Sign In</button>
-                    <button onClick={() => dispatch(modalClose(false))} className="navbar_btn">Log out</button>
+
+                    {currentUser
+                        ? <div>
+                            <Link to='/private/private-home'>
+                                <h3>{currentUser.email}</h3>
+                            </Link>
+                            <button onClick={() => logOut()}>Logout</button>
+                        </div>
+                        : <div> {!loadingData && <>
+                            <button onClick={() => dispatch(signInToggle(true))} className="navbar_btn">Login</button>
+                            <button onClick={() => dispatch(signUpToggle(true))} className="navbar_btn">Sign Up</button>
+                        </>
+                        }
+                        </div>
+                    }
+
+
+
                 </div>
             </div>
         </nav >
